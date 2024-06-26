1a5fc1d4b1348339c066acdf2b7755b670e12130
hemanthk1 <hemanth.k@like.digital>
2024-06-26 11:45:42 +0530

    fix: headerfooter styles

--- src/components/module/headerFooter/index.tsx
+++ src/components/module/headerFooter/index.tsx
@@ -4,7 +4,7 @@ import NavigationLink from "@components/module/navigationLink";
 
 const footerLinks = [
   { id: 1, label: "Contact" },
-  { id: 2, label: "store locator" },
+  { id: 2, label: "Store locator" },
   { id: 3, label: "About Us" },
 ];
 
@@ -12,7 +12,12 @@ const HeaderFooter = ({ className }: { className?: any }) => {
   return (
     <div className={`${className} ${styles.container}`}>
       {footerLinks.map((data) => (
-        <NavigationLink title={data.label} key={data.id} />
+        <NavigationLink
+          hover={true}
+          className={styles.footerNavigation}
+          title={data.label}
+          key={data.id}
+        />
       ))}
     </div>
   );
