f6b03b93aad23ff023cf9b8cc77a5b19910b2df0
priyankdeep <priyankdeepsingh@gmail.com>
2024-07-17 10:23:42 +0530

    updated page file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { FormEvent } from "react";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { SideDrawer } from "@components/module";
 import Layout from "@components/layout";
 import ContentBlock from "@components/module/contentBlock";
@@ -9,10 +9,8 @@ import fetchStandardPageData from "@utils/cms/page/fetchStandardPageData";
 
 import { getCustomer } from "@utils/sfcc-connector/dataService";
 import LoginForm from "@components/LoginForm";
-import RegistrationForm from "@components/RegistrationForm";
-import PriceRangeFilter from "@components/module/priceRangeFilter";
-import { getHierarchyChildren } from "@utils/cms/amplience";
-import Accordion from "@components/module/accordion";
+import RegistrationForm from "@components/RegistrationForm"; 
+import { getHierarchyChildren } from "@utils/cms/amplience"; 
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const data = await fetchStandardPageData(
@@ -26,34 +24,14 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 
   return {
     props: {
-      ...data
+      ...data,
     },
   };
 }
 
-const accordionItem = {
-  id: "priceRange",
-  title: "Price",
-};
-
 export default function Home({ contents }) {
-  const [isDrawerOpen, setDrawerOpen] = useState(false);
-  const [subMenu, setSubMenu] = useState<string | null>(null);
-
-  const toggleDrawer = () => setDrawerOpen(!isDrawerOpen);
   return (
     <div className="main-content">
-     <button onClick={toggleDrawer}>Open Drawer</button>
-      <SideDrawer isOpen={isDrawerOpen} onClose={() => setDrawerOpen(false)}>
-       <Accordion 
-        item={accordionItem} 
-        setSubMenu={setSubMenu} 
-        subMenu={subMenu} 
-        showArrow={true}
-      >
-        <PriceRangeFilter />
-      </Accordion>
-      </SideDrawer>
       {compact(contents).map((content) => (
         <ContentBlock content={content} key={content?._meta.deliveryId} />
       ))}
