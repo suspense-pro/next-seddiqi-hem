36ab315c2bdf3674297985468cdb0e2c0cde141f
priyankdeep <priyankdeepsingh@gmail.com>
2024-08-23 13:07:38 +0530

    uopdated width

--- src/components/rendering/boutiqueBanner/index.tsx
+++ src/components/rendering/boutiqueBanner/index.tsx
@@ -3,7 +3,7 @@ import styles from "./boutiqueBanner.module.scss";
 import Typography from "../../module/typography";
 import RichText from "../../module/richText";
 import { Image } from "@components/module";
-import Video from "@components/module/video"; 
+import Video from "@components/module/video";
 import Button from "../../module/button";
 import { BoutiqueBannerProps } from "@utils/models";
 
@@ -23,26 +23,32 @@ const BoutiqueBanner: React.FC<BoutiqueBannerProps> = ({
 
   return (
     <div className={`${styles.boutiqueBannerContainer} ${bgClass}`}>
-      <div className={styles.mediaContainer}>
-        {media?.image ? (
-          <Image image={media.image} imageAltText={media.altText} />
-        ) : media?.video ? (
-          <Video video={media.video} autoPlay={media.autoPlay} showPlay={media.showPlay} />
-        ) : null}
-      </div>
-      <div className={styles.contentContainer}>
-        <Typography variant="h2" className={styles.title}>
-          {mainTitle}
-        </Typography>
-        <div className={styles.richTextContainer}>
-          <RichText align="left" text={richText} />
+      <div className={styles.innerContainer}>
+        <div className={styles.mediaContainer}>
+          {media?.image ? (
+            <Image image={media.image} imageAltText={media.altText} />
+          ) : media?.video ? (
+            <Video
+              video={media.video}
+              autoPlay={media.autoPlay}
+              showPlay={media.showPlay}
+            />
+          ) : null}
         </div>
-        <div className={styles.ctaContainer}>
-          <Button
-            title={cta?.label}
-            type={cta?.type}
-            className={styles.ctaButton}
-          />
+        <div className={styles.contentContainer}>
+          <Typography variant="h2" className={styles.title}>
+            {mainTitle}
+          </Typography>
+          <div className={styles.richTextContainer}>
+            <RichText align="left" text={richText} />
+          </div>
+          <div className={styles.ctaContainer}>
+            <Button
+              title={cta?.label}
+              type={cta?.type}
+              className={styles.ctaButton}
+            />
+          </div>
         </div>
       </div>
     </div>
