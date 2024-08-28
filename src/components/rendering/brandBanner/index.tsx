5528c11534b073f579ca5a14cce1855b6f374b3b
priyankdeep <priyankdeepsingh@gmail.com>
2024-08-28 10:54:36 +0530

    brand banner  bug fix

--- src/components/rendering/brandBanner/index.tsx
+++ src/components/rendering/brandBanner/index.tsx
@@ -4,7 +4,7 @@ import Typography from "../../module/typography";
 import RichText from "../../module/richText";
 import { Image } from "@components/module";
 import { BrandBannerProps } from "@utils/models";
-import BrandBannerIcon from "@assets/images/svg/BrandBannerIcon"; 
+import BrandBannerIcon from "@assets/images/svg/BrandBannerIcon";
 
 const BrandBanner: React.FC<BrandBannerProps> = ({
   mainTitle,
@@ -13,16 +13,16 @@ const BrandBanner: React.FC<BrandBannerProps> = ({
 }) => {
   return (
     <div className={styles.brandBannerContainer}>
-        <div className={styles.brandBannerIcon}>
-          <BrandBannerIcon /> 
-        </div>
+      <div className={styles.brandBannerIcon}>
+        <BrandBannerIcon />
+      </div>
       <div className={styles.textContainer}>
         <div className={styles.innerContent}>
           <Typography variant="p" className={styles.title}>
-            {mainTitle}
+            {mainTitle?.toUpperCase()}
           </Typography>
           <div className={styles.richTextContainer}>
-            <RichText align="left" text={richText} />
+            <RichText align="" text={richText} />
           </div>
         </div>
       </div>
