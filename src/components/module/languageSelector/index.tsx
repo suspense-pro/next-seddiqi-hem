30cd32d873a9437d2cd91a7f3574ef36adb9ca9d
priyankdeep <priyankdeepsingh@gmail.com>
2024-10-11 11:10:06 +0530

    added country language dropdown in footer

--- src/components/module/languageSelector/index.tsx
+++ src/components/module/languageSelector/index.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { LanguageContext, LANGUAGE_DICT } from "@contexts/languageContext";
+import { LanguageContext, LANGUAGE_DICT, COUNTRY_DICT } from "@contexts/languageContext";
 import styles from "./languageSelector.module.scss";
 import { ArrowDown } from "@assets/images/svg";
 
@@ -8,47 +8,48 @@ interface LanguageSelectorProps {
 }
 
 const LanguageSelector: React.FC<LanguageSelectorProps> = ({ className }) => {
-  const { language, handleLanguageChange } = useContext(LanguageContext);
-  const getCountryName = (code: string) => {
-    if (code === "ar") {
-      return "UAE";
-    }
-    return LANGUAGE_DICT[code];
-  };
+  const {
+    language,
+    country,
+    handleLanguageChange,
+    handleCountryChange,
+  } = useContext(LanguageContext);
+
   return (
     <div className={`${styles.selection} ${className}`}>
       <label className={styles.label}>COUNTRY & LANGUAGE</label>
-      <div className={`${styles.dropdown} ${styles.countryDropdown}`}>
-        <div className={styles.customSelect}>
-          <select
-            onChange={(e) => handleLanguageChange?.(e.target.value)}
-            value={language || "en"}
-          >
-            {" "}
-            {Object.entries(LANGUAGE_DICT).map(([key, name]) => (
-              <option key={key} value={key}>
-                {getCountryName(key)}
-              </option>
-            ))}
-          </select>
-          <ArrowDown className={styles.arrow} />
+      <div className={styles.dropdowns}>
+        <div className={`${styles.dropdown} ${styles.countryDropdown}`}>
+          <div className={styles.customSelect}>
+            <select
+              onChange={(e) => handleCountryChange?.(e.target.value)}
+              value={country || "ae"}
+            >
+              {Object.entries(COUNTRY_DICT).map(([key, name]) => (
+                <option key={key} value={key}>
+                  {name}
+                </option>
+              ))}
+            </select>
+            <ArrowDown className={styles.arrow} />
+          </div>
+        </div>
+        <div className={`${styles.dropdown} ${styles.languageDropdown}`}>
+          <div className={styles.customSelect}>
+            <select
+              onChange={(e) => handleLanguageChange?.(e.target.value)}
+              value={language || "en"}
+            >
+              {Object.entries(LANGUAGE_DICT).map(([key, name]) => (
+                <option key={key} value={key}>
+                  {name}
+                </option>
+              ))}
+            </select>
+            <ArrowDown className={styles.arrow} />
+          </div>
         </div>
       </div>
-      {/* <div className={`${styles.dropdown} ${styles.languageDropdown}`}>
-        <div className={styles.customSelect}>
-          <select
-            onChange={(e) => handleLanguageChange?.(e.target.value)}
-            value={language || "en"}
-          > 
-            {Object.entries(LANGUAGE_DICT).map(([key, name]) => (
-              <option key={key} value={key}>
-                {name}
-              </option>
-            ))}
-          </select>
-          <ArrowDown className={styles.arrow} />
-        </div>
-      </div> */}
     </div>
   );
 };
