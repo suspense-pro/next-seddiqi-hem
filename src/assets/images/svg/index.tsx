15df7015940de0a9cd41328ab7a433275c1f1412
priyankdeep <priyankdeepsingh@gmail.com>
2024-10-14 00:04:51 +0530

    remove pin icon and updated step3

--- src/assets/images/svg/index.tsx
+++ src/assets/images/svg/index.tsx
@@ -29,7 +29,6 @@ export { default as CubeIcon } from "./CubeIcon";
 export { default as WatchIcon } from "./WatchIcon";
 export { default as ProtectionIcon } from "./ProtectionIcon";
 export { default as CareIcon } from "./CareIcon";
-
 export { default as MailIcon } from "./MailIcon";
 export { default as PhoneIcon } from "./PhoneIcon";
 export { default as TimeIcon } from "./TimeIcon";
