07d457b9c639686f8e8df6793a1e0a21d2b4c03e
priyankdeep <priyankdeepsingh@gmail.com>
2024-10-18 14:31:37 +0530

    commenteed bugs

--- src/components/rendering/heroBanner/index.tsx
+++ src/components/rendering/heroBanner/index.tsx
@@ -50,7 +50,6 @@ const HeroBanner: React.FC<HeroBannerProps> = ({ banners, bannerType }) => {
     activeBanner?.horizontalAlignment === "right"
       ? "left"
       : activeBanner?.horizontalAlignment || "center";
-
   return (
     <>
       <div className={`${styles.heroBanner} ${containerClass}`}>
@@ -107,7 +106,7 @@ const HeroBanner: React.FC<HeroBannerProps> = ({ banners, bannerType }) => {
                 <div className={styles.ctaButton}>
                   <Button
                     title={activeBanner.cta[0]?.label}
-                    type={activeBanner.cta[0]?.type}
+                    type={`${activeBanner.cta[0]?.type?.toLowerCase()} ${activeBanner.cta[0]?.color?.toLowerCase()}`}
                   />
                 </div>
               )}
