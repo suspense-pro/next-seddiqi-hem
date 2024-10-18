ce680b976e50ea4ffd2059d548267304d4ecb0c0
hemanthk1 <hemanth.k@like.digital>
2024-10-18 15:52:04 +0530

    header fixies

--- src/components/rendering/header/index.tsx
+++ src/components/rendering/header/index.tsx
@@ -1,13 +1,9 @@
 import { HeaderProvider } from "@contexts/headerContext";
 import styles from "./header.module.scss";
 import { DesktopHeader, MegaMenu, MobileHeader } from "@components/module";
-import { useDeviceWidth } from "@utils/useCustomHooks";
+import { useWindowWidth } from "@utils/useCustomHooks";
 
 export default function Header({ ...props }) {
-  const isDesktop = useDeviceWidth()[0];
-  return (
-    <header className={styles.header}>
-      {isDesktop ? <DesktopHeader /> : <MobileHeader />}
-    </header>
-  );
+  const windowWidth = useWindowWidth();
+  return <header className={styles.header}>{windowWidth > 1036 ? <DesktopHeader /> : <MobileHeader />}</header>;
 }
