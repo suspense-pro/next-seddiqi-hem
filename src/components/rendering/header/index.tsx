b096cb846efc308f664a9146da40a98fdea95708
hemanthk1 <hemanth.k@like.digital>
2024-10-18 18:53:11 +0530

    background color change for product carousel

--- src/components/rendering/header/index.tsx
+++ src/components/rendering/header/index.tsx
@@ -2,8 +2,17 @@ import { HeaderProvider } from "@contexts/headerContext";
 import styles from "./header.module.scss";
 import { DesktopHeader, MobileHeader } from "@components/module";
 import { useWindowWidth } from "@utils/useCustomHooks";
+import { useEffect, useState } from "react";
 
 export default function Header({ ...props }) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   const windowWidth = useWindowWidth();
-  return <header className={styles.header}>{windowWidth > 1036 ? <DesktopHeader /> : <MobileHeader />}</header>;
+  return (
+    <header className={styles.header}>{isClient && (windowWidth > 1036 ? <DesktopHeader /> : <MobileHeader />)}</header>
+  );
 }
