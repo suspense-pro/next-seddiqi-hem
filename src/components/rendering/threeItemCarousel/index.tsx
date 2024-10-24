f12b1f863d1ea574e5f8b820b4c3f7416546e231
hemanthk1 <hemanth.k@like.digital>
2024-10-24 19:53:32 +0530

    category list mobile height fix

--- src/components/rendering/threeItemCarousel/index.tsx
+++ src/components/rendering/threeItemCarousel/index.tsx
@@ -85,7 +85,7 @@ const ThreeItemCarousel = ({ mainTitle, hideUnderline, richText, listItems = [] 
                 link={item.cta.url}
                 className={styles.discoverBtn}
                 title={item.cta.label}
-                color={item.cta.color}
+                color={"black"}
                 type={item.cta.type}
               />
             </div>
