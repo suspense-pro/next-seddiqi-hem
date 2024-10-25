fb62b1aa5f2432a619540013308ece47fae59557
abuzer-arshad <abuzer.a@like.digital>
2024-10-25 19:40:06 +0100

    SMTP email helper: account registration email and OTPs email flow

---
+++ src/utils/helpers/emailHelper.ts
@@ -0,0 +1,33 @@
+// emailHelper.ts
+import nodemailer from 'nodemailer';
+
+export const sendEmail = async (to: string, subject: string, htmlContent: string) => {
+  // Configure the transporter
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: false,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+  // Email options
+  const mailOptions = {
+    from: process.env.SMTP_USER, // Sender address
+    to,
+    subject,
+    html: htmlContent,
+  };
+
+  // Send email
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent: %s', info.messageId);
+    return { success: true, message: 'Email sent successfully' };
+  } catch (error) {
+    console.error('Error sending email:', error);
+    return { success: false, message: 'Failed to send email' };
+  }
+};
