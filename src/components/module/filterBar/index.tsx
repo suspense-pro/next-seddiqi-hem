50e31593c3e689ebb57b7f0e9f8f7e0c81b7d272
jldigital <joaquin@like.digital>
2024-11-01 00:47:54 +0800

    initial bugfix plp

--- src/components/module/filterBar/index.tsx
+++ src/components/module/filterBar/index.tsx
@@ -11,52 +11,60 @@ import Typography from "../typography";
 import { CloseIcon } from "@assets/images/svg";
 import { getCategoryFilters } from "@utils/sfcc-connector/dataService";
 
-const FILTERS = [
-  { icon: true, text: "All Filter & Sort by" },
-  { icon: false, text: "New" },
-  { icon: false, text: "Exclusive" },
-  { icon: false, text: "Gifts" },
-  { icon: false, text: "Messika" },
-  { icon: false, text: "Bvlgari" },
-];
+// const FILTERS = [
+//   { icon: true, text: "All Filter & Sort by" },
+//   { icon: false, text: "New" },
+//   { icon: false, text: "Exclusive" },
+//   { icon: false, text: "Gifts" },
+//   { icon: false, text: "Messika" },
+//   { icon: false, text: "Bvlgari" },
+// ];
 
 const FilterBar = ({
   filters: initialFilters,
   onFilterChange,
   totalProducts,
-  categoryId,
+  filterOptions,
+  sortingOptions,
+  quickFilters
 }) => {
   const [isDrawerOpen, setDrawerOpen] = useState(false);
   const [filters, setFiltersState] = useState(initialFilters || {});
-  const [filterOptions, setFilterOptions] = useState([]);
-  const [sortingOptions, setSortingOptions] = useState([]);
+  // const [filterOptions, setFilterOptions] = useState([]);
+  // const [sortingOptions, setSortingOptions] = useState([]);
+  // const [quickFilters, setQuickFilters] = useState([]);
 
-  useEffect(() => {
-    const fetchCategoryFilters = async () => {
-      try {
-        const response = await getCategoryFilters({
-          method: "GET",
-          cgid: categoryId,
-        });
-        // console.log("response-------", response);
-        if (response && response.refinements) {
-          setFilterOptions(response.refinements);
-        }
+  // useEffect(() => {
+    
+  //   const fetchCategoryFilters = async () => {
+  //     try {
+  //       const response = await getCategoryFilters({
+  //         method: "GET",
+  //         cgid: categoryId,
+  //       });
+  //       console.log("response-------", response);
+  //       if (response && response.refinements) {
+  //         setFilterOptions(response.refinements);
+  //       }
 
-        if (response && response.sortingOptions) {
-          setSortingOptions(response.sortingOptions);
-        }
-      } catch (error) {
-        console.error("error-", error);
-      }
-    };
+  //       if (response && response.sortingOptions) {
+  //         setSortingOptions(response.sortingOptions);
+  //       }
 
-    fetchCategoryFilters();
-  }, [categoryId]);
+  //       if (response && response.quickFilters) {
+  //         setQuickFilters(response.quickFilters);
+  //       }
+  //     } catch (error) {
+  //       console.error("error-", error);
+  //     }
+  //   };
 
-  useEffect(() => {
-    setFiltersState(initialFilters || {});
-  }, [initialFilters]);
+  //   fetchCategoryFilters();
+  // }, [categoryId]);
+
+  // useEffect(() => {
+  //   setFiltersState(initialFilters || {});
+  // }, [initialFilters]);
 
   useEffect(() => {
     if (!isDrawerOpen) {
@@ -77,6 +85,8 @@ const FilterBar = ({
 
       return { ...prevFilters, [filterKey]: newSelectedOptions };
     });
+
+    onFilterChange(filters);
   };
 
   const handleSortChange = (selectedSortOption) => {
@@ -121,7 +131,7 @@ const FilterBar = ({
     if (onFilterChange) {
       onFilterChange(filteredFilters);
     }
-    setDrawerOpen(false);
+
   };
 
   const handleClearAll = () => {
@@ -151,22 +161,25 @@ const FilterBar = ({
   return (
     <div className={styles.container}>
       <div className={styles.filterBtns}>
-        {FILTERS.map((item, index) => (
           <FilterBtn
-            key={index}
-            label={item.text}
-            icon={item.icon}
-            onClick={
-              item.text === "All Filter & Sort by" ? toggleDrawer : undefined
-            }
+            label={"All Filter"}
+            icon={true}
+            onClick={toggleDrawer}
           />
-        ))}
+          {quickFilters.map((item, index) => (
+            <FilterBtn
+              key={index}
+              label={item.label}
+              icon={false}
+              onClick={undefined}
+            />
+          ))}
       </div>
       <div className={styles.productsLength}>{totalProducts} Products</div>
       <SideDrawer
         isOpen={isDrawerOpen}
         onClose={() => setDrawerOpen(false)}
-        onSubmit={handleSubmit}
+        onSubmit={null}
         onClearAll={handleClearAll}
         showFooter={true}
         showBackButton={false}
@@ -213,7 +226,7 @@ const FilterBar = ({
         </div>
 
         <FilterAccordian>
-          <FilterAccordionItem 
+          {/* <FilterAccordionItem 
             title="Sort"
             onClear={() => handleClearCheckboxes("sortOption")}
             selectedCount={filters.sortOption ? 1 : 0}
@@ -223,7 +236,7 @@ const FilterBar = ({
               selectedSortOption={filters.sortOption}
               onSortChange={handleSortChange}
             />
-          </FilterAccordionItem>
+          </FilterAccordionItem> */}
           {filterOptions.map((filterItem) => (
             <FilterAccordionItem
               key={filterItem.attributeId}
@@ -231,7 +244,7 @@ const FilterBar = ({
               onClear={() => handleClearCheckboxes(filterItem.attributeId)}
               selectedCount={filters[filterItem.attributeId]?.length || 0}
             >
-              {filterItem.values && (
+              {filterItem.values &&  (!filterItem.attributeId.includes('color') && !filterItem.attributeId.includes('price')) && (
                 <CheckboxFilter
                   title={filterItem.label}
                   options={filterItem.values.map((val) => val.label)}
@@ -240,9 +253,13 @@ const FilterBar = ({
                   selectedOptions={filters[filterItem.attributeId] || []}
                 />
               )}
+
+              {filterItem.attributeId.includes('price') && <PriceRangeFilter />}
+              {filterItem.attributeId.includes('color') &&   <ColorFilter />}
+
             </FilterAccordionItem>
           ))}
-          <FilterAccordionItem 
+          {/* <FilterAccordionItem 
             title="Price"
             onClear={() => handleClearCheckboxes("price")}
             selectedCount={filters.price ? 1 : 0}
@@ -255,7 +272,7 @@ const FilterBar = ({
             selectedCount={filters.color ? 1 : 0}
           >
             <ColorFilter />
-          </FilterAccordionItem>
+          </FilterAccordionItem> */}
         </FilterAccordian>
       </SideDrawer>
     </div>
--- src/components/module/plpContent/index.tsx
+++ src/components/module/plpContent/index.tsx
@@ -1,15 +1,16 @@
 import React, { useState, useRef, useEffect } from "react";
 import { useRouter } from "next/router";
 import FilterBar from "../filterBar";
-import styles from "./productsContnet.module.scss";
+import styles from "./productsContent.module.scss";
 import GridWrapper from "../gridWrapper";
 import ProductCard from "../cards/productCard";
 import { ComponentMapping } from "@utils/cms/config";
 import { generateUniqueId } from "@utils/helpers/uniqueId";
 import Button from "../button";
-import { setFilters } from "@utils/sfcc-connector/dataService";
+import { getCategoryFilters, setFilters } from "@utils/sfcc-connector/dataService";
 import Typography from "../typography";
 import Loader from "../loader";
+import ScrollToTop from "../scrollToTop";
 
 const LOAD_MORE_TEXT = "Load More";
 
@@ -19,23 +20,21 @@ const PlpContent = ({ productGridContent, products }) => {
   if (!products) return null;
 
   const router = useRouter();
-
-  const categoryId =
-    products?.query?.TermQuery?.values?.[0] || "";
-
-    const allHits = Array.isArray(products) ? products : products?.hits || [];
+  const categoryId = products?.query?.TermQuery?.values?.[0] || "";
+  const allHits = Array.isArray(products) ? products : products?.hits || [];
 
   const [filters, setFiltersState] = useState(null);
-  const [displayedProducts, setDisplayedProducts] = useState(
-    allHits.slice(0, 24)
-  );
+  const [displayedProducts, setDisplayedProducts] = useState(allHits.slice(0, 24));
   const [currentIndex, setCurrentIndex] = useState(24);
   const [isAllLoaded, setIsAllLoaded] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const productsRef = useRef(null);
-  const isButtonDisabled = displayedProducts.length <= 24 || isAllLoaded;
-
   const [hasInitializedFilters, setHasInitializedFilters] = useState(false);
+  const [filterOptions, setFilterOptions] = useState([]);
+  const [sortingOptions, setSortingOptions] = useState([]);
+  const [quickFilters, setQuickFilters] = useState([]);
+
+  const productsRef = useRef(null);
+  const isButtonDisabled = products?.total <= 24 || isAllLoaded;
 
   const loadMoreProducts = () => {
     const currentScrollPos = window.scrollY;
@@ -168,6 +167,34 @@ const PlpContent = ({ productGridContent, products }) => {
     fetchFilteredProducts();
   }, [filters, categoryId]);
 
+  useEffect(() => {
+    
+    const fetchCategoryFilters = async () => {
+      try {
+        const response = await getCategoryFilters({
+          method: "GET",
+          cgid: categoryId,
+        });
+        console.log("response-------", response);
+        if (response && response.refinements) {
+          setFilterOptions(response.refinements);
+        }
+
+        if (response && response.sortingOptions) {
+          setSortingOptions(response.sortingOptions);
+        }
+
+        if (response && response.quickFilters) {
+          setQuickFilters(response.quickFilters);
+        }
+      } catch (error) {
+        console.error("error-", error);
+      }
+    };
+
+    fetchCategoryFilters();
+  }, [categoryId]);
+
   const totalProducts = products?.total || allHits.length;
   const PRODUCT_INFO_TEXT = `Showing ${displayedProducts.length} out of ${totalProducts} products`;
 
@@ -178,7 +205,9 @@ const PlpContent = ({ productGridContent, products }) => {
           filters={filters || {}}
           onFilterChange={setFiltersState}
           totalProducts={totalProducts}
-          categoryId={categoryId}
+          filterOptions={filterOptions}
+          sortingOptions={sortingOptions}
+          quickFilters={quickFilters}
         />
 
         {displayedProducts.length > 0 ? (
@@ -208,14 +237,14 @@ const PlpContent = ({ productGridContent, products }) => {
             <div className={styles.bottom}>
               <div className={styles.productInfo}>{PRODUCT_INFO_TEXT}</div>
               {!isAllLoaded && (
-                <div>
+                // <div>
                   <Button
                     title={LOAD_MORE_TEXT}
-                    type="solid green_dark"
+                    type="solid metallic"
                     disabled={isButtonDisabled}
                     clickHandler={loadMoreProducts}
                   />
-                </div>
+                // </div>
               )}
             </div>
           </>
@@ -227,6 +256,7 @@ const PlpContent = ({ productGridContent, products }) => {
 
         {isLoading && <Loader />}
       </div>
+      <ScrollToTop />
     </div>
   );
 };
