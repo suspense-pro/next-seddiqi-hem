146f4711ee4ec2f6bf790e2d130f4971ab582916
priyankdeep <priyankdeepsingh@gmail.com>
2024-11-06 17:21:47 +0530

    minor

--- src/components/rendering/rolex/rolexProduxtLister/index.tsx
+++ src/components/rendering/rolex/rolexProduxtLister/index.tsx
@@ -48,7 +48,7 @@ const RolexProductLister = (content) => {
                   </div>
                   <div className={styles.productInfo}>
                     <Typography variant="p" className={styles.productTitle}>
-                      {title}--000
+                      {title}
                     </Typography>
                     <Typography variant="p" className={styles.productName}>
                       {name}
