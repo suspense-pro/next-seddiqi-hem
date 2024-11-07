793e69279db42c7fbcca2d6862810d00d46cd612
jldigital <joaquin@like.digital>
2024-11-07 21:35:45 +0800

    header only logos fix

--- src/components/module/mobileMenuLogobar/index.tsx
+++ src/components/module/mobileMenuLogobar/index.tsx
@@ -15,14 +15,7 @@ const MobileMenuLogobar = () => {
   if(!PATEK_LOGO || !ROLEX_LOGO) return null
 
   const RolexScript = 
-  ` <div id="rolex-retailer-clock">
-      <div class="LinkEnabler">
-      <!-- Container div for scaling -->
-      <div class="clockContainer">
-      <iframe id="rolex_retailer" title="Rolex Official Retailer" src="
-      https://static.rolex.com/retailers/clock/?colour=gold&amp;apiKey=fd5d8663fc8674a9ffab32649c3e39bf&amp;lang=en" scrolling="no" frameborder="no"></iframe>
-      </div>
-    </div>`
+  `<div id="rolex-retailer-clock"><div class="LinkEnabler" style="position:absolute; height:70px; width:150px; z-index:1;"></div><iframe id="rolex_retailer" title="Rolex Official Retailer" src="https://static.rolex.com/retailers/clock/?colour=gold&amp;apiKey=fd5d8663fc8674a9ffab32649c3e39bf&amp;lang=en" style="width:150px;height:70px;border:0;margin:0;padding:0;overflow:hidden;z-index:0;position:relative;scroll:none" scrolling="NO" frameborder="NO"></iframe></div>`
     
   return (
     <div className={styles.bottom}>
@@ -40,8 +33,8 @@ const MobileMenuLogobar = () => {
           <Link href={"/"}>
             <Image
               src={PATEK_LOGO}
-              width={82}
-              height={48}
+              width={118}
+              height={68}
               alt={leftLogo?.image?.altText}
               className={styles.image}
             />
