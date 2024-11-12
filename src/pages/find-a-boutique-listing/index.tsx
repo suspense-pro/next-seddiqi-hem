7b5b7b296cbce54184a32b6d47ccc0cb77e667aa
jldigital <joaquin@like.digital>
2024-11-12 18:44:26 +0800

    need more help fix

--- src/pages/find-a-boutique-listing/index.tsx
+++ src/pages/find-a-boutique-listing/index.tsx
@@ -528,7 +528,7 @@ export default function FindABoutiqueListing({ content }: InferGetServerSideProp
         </div>
       </div>
 
-      <NeedMoreHelp content={content.page.needMoreHelp} />
+      <NeedMoreHelp {...content.page.needMoreHelp} />
 
       <SideDrawer
         isOpen={filtersPopup}
