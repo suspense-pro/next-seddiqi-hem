d4e3027bb85a7c29a98afb272c63c9573f6390e2
hemanthk1 <hemanth.k@like.digital>
2024-11-13 15:19:13 +0530

    bugfix content block

--- src/components/module/contactForm/index.tsx
+++ src/components/module/contactForm/index.tsx
@@ -17,6 +17,7 @@ import { countryCodes } from "@utils/data/countryCodes";
 const ContactForm = () => {
   const router = useRouter();
   const [attachment, setAttachment] = useState<File | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [formData, setFormData] = useState({
     topic: "Compliant",
     orderNumber: "",
@@ -40,8 +41,10 @@ const ContactForm = () => {
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
+    // console.log("HELLO")
 
     if (validateForm()) {
+      setIsLoading(true);
       const userData = {
         type: formData.topic,
         orderReferenceNumber: formData.orderNumber,
@@ -60,15 +63,22 @@ const ContactForm = () => {
         data?.append("attachment", attachment);
       }
 
-      const response = await contactUs({
-        method: "POST",
-        userData: data,
-      });
+      try {
+        const response = await contactUs({
+          method: "POST",
+          userData: data,
+        });
+        console.log("HELLO");
 
-      if (!response?.isError) {
-        router.push("/contact-us/confirmation");
-      } else {
-        alert("Failed to send email");
+        if (!response?.isError) {
+          router.push("/contact-us/confirmation");
+        } else {
+          alert("Failed to send email");
+        }
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
       }
     }
   };
@@ -245,10 +255,11 @@ const ContactForm = () => {
         <Button
           clickHandler={handleSubmit}
           className={styles.submitBtn}
-          title="Submit"
+          title={isLoading ? "Submitting" : "Submit"}
           isLink={false}
           type="solid"
           color="metallic"
+          disabled={isLoading}
         />
       </div>
     </form>
