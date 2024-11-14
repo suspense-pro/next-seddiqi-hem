1810a0c44a6f162554bf6f8e2252d38b8888a384
hemanthk1 <hemanth.k@like.digital>
2024-11-14 19:46:39 +0530

    news letter remove

--- src/components/rendering/footer/index.tsx
+++ src/components/rendering/footer/index.tsx
@@ -47,8 +47,6 @@ export default function Footer({ footerData }: FooterPropType) {
       <div className={styles.container}>
         <div className={styles.column} style={{ gridArea: "logo" }}>
           <Image src={logoUrl} alt={logoAltText} width={209} height={44} className={styles.logo} />
-
-         
         </div>
         <div className={styles.rightLinks}>
           <div className={styles.linkContainer}>
