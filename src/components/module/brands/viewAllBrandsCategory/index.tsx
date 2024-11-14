72ce4e5d05471a8153292611693ff01efb43dcba
jldigital <joaquin@like.digital>
2024-11-15 00:26:56 +0800

    brand lister fix

--- src/components/module/brands/viewAllBrandsCategory/index.tsx
+++ src/components/module/brands/viewAllBrandsCategory/index.tsx
@@ -4,19 +4,18 @@ import styles from "./viewAllBrandsCategory.module.scss";
 import BrandListing from "@components/module/brandListing";
 
 const ViewAllBrandsCategory = ({ ...content }) => {
-  console.log("ViewAllBrandsCategory", content);
+  // console.log("ViewAllBrandsCategory", content);
   return (
-    <div>
+
       <TabbedNavigation
         tabs={[
-          { id: 1, title: "All brands", content: <BrandListing height={false} /> },
-          { id: 2, title: "watches" },
-          { id: 3, title: "Jewellery" },
-          { id: 4, title: "accessories" },
+          { id: 1, title: "All brands" },
+          // { id: 2, title: "watches" },
+          // { id: 3, title: "Jewellery" },
+          // { id: 4, title: "accessories" },
         ]}
-        className={styles.tabContainer}
+        // className={styles.tabContainer}
       />
-    </div>
   );
 };
 
