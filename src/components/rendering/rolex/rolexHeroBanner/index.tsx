a7e18bd4329f88f03c2170d1074ab62ca95067ab
hemanthk1 <hemanth.k@like.digital>
2024-11-15 22:11:00 +0530

    updated rolex hero banner

--- src/components/rendering/rolex/rolexHeroBanner/index.tsx
+++ src/components/rendering/rolex/rolexHeroBanner/index.tsx
@@ -15,6 +15,8 @@ const RolexHeroBanner = ({ ...content }) => {
 
   if (!content) return null;
 
+  console.log("content", content);
+
   const slides = content?.listItems;
 
   const handleSlideChange = (swiper) => {
@@ -27,7 +29,6 @@ const RolexHeroBanner = ({ ...content }) => {
 
   const bannerType = content?.listItems[0]?.bannerType;
 
-  const screenSize = useWindowWidth();
   return (
     <Swiper
       ref={swiperRef}
@@ -72,8 +73,8 @@ const RolexHeroBanner = ({ ...content }) => {
               )}
               {slide?.bannerType === "Rolex" ? (
                 <div className={styles.textOverlay}>
-                  <div className={styles.brand}>{slide?.title}</div>
-                  <h2 className={styles.title}>{slide.subHeading}</h2>
+                  {slide?.title && <div className={styles.brand}>{slide?.title}</div>}
+                  {slide.subHeading && <h2 className={styles.title}>{slide.subHeading}</h2>}
                   {slide?.cta?.label && (
                     <div className={styles.btnContainer}>
                       <NavigationLink
@@ -86,7 +87,7 @@ const RolexHeroBanner = ({ ...content }) => {
                   )}
                 </div>
               ) : (
-                <div className={styles.textOverlayCpo}>
+                <div className={`${slide?.textPosition === "Center" && styles.centerCpo} ${styles.textOverlayCpo}`}>
                   {slide?.title && <div className={styles.brand}>{slide?.title}</div>}
 
                   {slide?.subHeading && <h2 className={styles.title}>{slide?.subHeading}</h2>}
