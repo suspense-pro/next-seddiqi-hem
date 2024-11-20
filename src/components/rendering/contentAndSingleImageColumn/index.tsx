065e20ffe4eb30b9007ea97946e9e3642db8ccfc
hemanthk1 <hemanth.k@like.digital>
2024-11-20 17:07:50 +0530

    added content and single image component

---
+++ src/components/rendering/contentAndSingleImageColumn/index.tsx
@@ -0,0 +1,23 @@
+import { ContentHeader, Image, Video } from "@components/module";
+import React from "react";
+import styles from "./contentAndSingleImageColumn.module.scss";
+
+const ContentAndSingleImageColumn = ({ ...content }) => {
+  console.log("content-and-single-image-column", content);
+  return (
+    <div className={styles.container}>
+      <ContentHeader
+        mainTitle={content?.mainTitle}
+        richText={content?.richText}
+        hideUnderline={content?.hideUnderline}
+      />
+      {content?.media?.image ? (
+        <Image height={styles.image} className={styles.image} image={content?.media?.image} />
+      ) : (
+        <Video className={styles.image} video={content?.media?.video} />
+      )}
+    </div>
+  );
+};
+
+export default ContentAndSingleImageColumn;
