316f549c77619812161dd6caac821501b7df7d0b
hemanthk1 <hemanth.k@like.digital>
2024-11-20 19:05:23 +0530

    added gloabl styles for background color

--- src/components/rendering/contentAndSingleImageColumn/index.tsx
+++ src/components/rendering/contentAndSingleImageColumn/index.tsx
@@ -4,7 +4,7 @@ import styles from "./contentAndSingleImageColumn.module.scss";
 
 const ContentAndSingleImageColumn = ({ ...content }) => {
   return (
-    <div className={`${styles[content?.backgroundColor]} ${styles.container}`}>
+    <div className={`${styles.container} ${content?.backgroundColor}`}>
       <ContentHeader
         mainTitle={content?.mainTitle}
         richText={content?.richText}
