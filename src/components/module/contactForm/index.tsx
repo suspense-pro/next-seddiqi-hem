ee2076fd111ba1bb1d1175ec1c7125268c4903b1
hemanthk1 <hemanth.k@like.digital>
2024-11-25 19:22:22 +0530

    contact us terms & conditions update

--- src/components/module/contactForm/index.tsx
+++ src/components/module/contactForm/index.tsx
@@ -256,7 +256,7 @@ const ContactForm = () => {
 
       <div className={styles.doubleForm}>
         <div className={`${styles.slidingSwitch}`}>
-          <SlidingRadioSwitch toggleLabel={""} onToggle={(value) => setAgreedToTerms(!value)} value={agreedToTerms} />
+          <SlidingRadioSwitch toggleLabel={""} onToggle={(value) => setAgreedToTerms(true)} value={agreedToTerms} />
           <p className={styles.switchLabel}>I agree to Ahmed Seddiqi & Sons Terms & Conditions and Privacy Policy.</p>
         </div>
         <div className={`${styles.slidingSwitch}`}>
