8765668349cda2a9797c74dbe2c4049ed4ff830f
hemanthk1 <hemanth.k@like.digital>
2024-12-16 12:42:36 +0530

    added rolex contact us page

--- src/components/rendering/rolex/rolexHeader/index.tsx
+++ src/components/rendering/rolex/rolexHeader/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, useCallback } from "react";
+import React, { useState, useRef, useEffect, useCallback, useContext } from "react";
 import styles from "./rolexHeader.module.scss";
 import Link from "next/link";
 import { ArrowDown } from "@assets/images/svg";
@@ -6,6 +6,7 @@ import { useWindowWidth } from "@utils/useCustomHooks";
 import Image from "../../../module/image/index";
 import NavigationLink from "../../../module/navigationLink";
 import { useRouter } from "next/router";
+import { RolexContext } from "@contexts/rolexContext";
 
 const RolexNavbar = ({ ...content }) => {
   if (!content) return null;
@@ -49,6 +50,7 @@ const RolexNavbar = ({ ...content }) => {
 
   if (!isClient) return null;
   const router = useRouter();
+  const { rolexContact, updateRolexContact } = useContext(RolexContext);
 
   return (
     <div className={styles.container}>
@@ -65,18 +67,35 @@ const RolexNavbar = ({ ...content }) => {
         {windowWidth > screenSize ? (
           <>
             <ul className={`${styles.navbarGreenLinks} ${styles.navbarLinks}`}>
-              {links?.map((link) => (
-                <li key={link?.label}>
-                  <NavigationLink
-                    className={`${link?.url === router?.asPath && styles.activeLink} ${isWhite && styles.navBlack} ${
-                      styles.navLink
-                    }`}
-                    title={link?.label}
-                    isNewTab={link?.isNewTab}
-                    url={link?.url}
-                  />
-                </li>
-              ))}
+              {links?.map((link) =>
+                link?.label?.toLowerCase() !== "contact us" ? (
+                  <li   onClick={() => {
+                    updateRolexContact(false);
+                  }} key={link?.label}>
+                    <NavigationLink
+                      className={`${isWhite && styles.navBlack} ${styles.navLink}`}
+                      title={link?.label}
+                      isNewTab={link?.isNewTab}
+                      url={link?.url}
+                    />
+                  </li>
+                ) : (
+                  <li
+                    onClick={() => {
+                      updateRolexContact(true);
+                    }}
+                    key={link?.label}
+                  >
+                    <div
+                      className={`${link?.url === router?.asPath && styles.activeLink} ${isWhite && styles.navBlack} ${
+                        styles.navLink
+                      }`}
+                    >
+                      <span>{link?.label}</span>
+                    </div>
+                  </li>
+                )
+              )}
             </ul>
             {/* <div className={styles.contactButton}>
               <NavigationLink
@@ -105,7 +124,7 @@ const RolexNavbar = ({ ...content }) => {
             }}
           >
             {links?.map((link, index) => (
-               <li onClick={() => setIsDropdownOpen(false)} key={index}>
+              <li onClick={() => setIsDropdownOpen(false)} key={index}>
                 <NavigationLink
                   className={`${isWhite && styles.navMobileBlack} ${styles.navMobileLink}`}
                   title={link?.label}
--- src/pages/rolex/index.tsx
+++ src/pages/rolex/index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent } from "react";
+import { FormEvent, useContext } from "react";
 import React, { useState } from "react";
 import { SideDrawer } from "@components/module";
 import Layout from "@components/layout";
@@ -9,8 +9,18 @@ import fetchStandardPageData from "@utils/cms/page/fetchStandardPageData";
 import { isEmpty } from "@utils/helpers";
 import { getProducts } from "@utils/sfcc-connector/dataService";
 import { RolexComponentMapping } from "@utils/cms/config";
-import { FooterBackToTop, ThreeCompactImageText, ThreeGrid, ThreeTallImageText, TwoColumnImageText } from "@components/rendering/rolex";
+import {
+  FooterBackToTop,
+  ThreeCompactImageText,
+  ThreeGrid,
+  ThreeTallImageText,
+  TwoColumnImageText,
+} from "@components/rendering/rolex";
 import ItemSlider from "@components/rendering/rolex/itemSlider";
+import { RolexContext, RolexProvider } from "@contexts/rolexContext";
+import ContactForm from "@components/module/contactForm";
+import RolexContactForm from "@components/rendering/rolex/rolexContactForm/rolexContactForm";
+import NeedMoreHelp from "@components/rendering/needMoreHelp";
 
 // import { getCustomer } from "@utils/sfcc-connector/dataService";
 // import LoginForm from "@components/LoginForm";
@@ -27,6 +37,17 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     context
   );
 
+  const needMoreHelp = await fetchStandardPageData(
+    {
+      content: {
+        page: {
+          key: "need-more-help",
+        },
+      },
+    },
+    context
+  );
+
   // if (isEmpty(data.content.page)) {
   //   return {
   //     redirect: {
@@ -37,20 +58,34 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   return {
     props: {
       ...data,
+      needMoreHelp
     },
   };
 }
 
-export default function RolexHome({ content }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  //console.log("Contents: ", content);
-
+export default function RolexHome({ content, needMoreHelp }) {
+  const { rolexContact } = useContext(RolexContext);
   return (
     <div className="main-content rolex">
-      {compact(content?.page?.components).map((content) => (
-        <ContentBlock components={RolexComponentMapping} content={content} key={content?._meta.deliveryId} />
-      ))}
-      
-      <FooterBackToTop contentImage={content?.page?.footerBlock?.footer?.media?.image} contentAlt={content?.page?.footerBlock?.footer?.media?.altText} />
+      {rolexContact ? (
+        <div>
+          {compact(content?.page?.components?.slice(0, 1)).map((content) => (
+            <ContentBlock components={RolexComponentMapping} content={content} key={content?._meta.deliveryId} />
+          ))}
+          <RolexContactForm />
+          {needMoreHelp?.content?.page && <NeedMoreHelp {...needMoreHelp?.content?.page} />}
+        </div>
+      ) : (
+        <>
+          {compact(content?.page?.components).map((content) => (
+            <ContentBlock components={RolexComponentMapping} content={content} key={content?._meta.deliveryId} />
+          ))}
+          <FooterBackToTop
+            contentImage={content?.page?.footerBlock?.footer?.media?.image}
+            contentAlt={content?.page?.footerBlock?.footer?.media?.altText}
+          />
+        </>
+      )}
     </div>
   );
 }
